5f2a36a31aa399363f54af0abd97eb6299fe9649
Eddie Ramirez <djedir@gmail.com>
2019-10-01 17:11:05 -0700

    Use HTTPS tunnel when kmdr-cli is behind a proxy (#20)
    * Enable kmdr client to work behind proxy.
    
    Since axxios has been forever broken on proxy aware
    I suggest to made the client proxy aware.
    
    Signed-off-by: Alejandro Joya <joya.a.cruz@gmail.com>
    
    * Use https tunnel when hitting an HTTPS endpoint
    
    - Use an HTTPS Tunnel only when the API endpoint is
      using protocol https and there's a HTTP_PROXY or
      HTTPS_PROXY environment variable set
    - Added new dependency tunnel
    - Linted code

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import { Agent } from "http";
 import os from "os";
+import Tunnel, { ProxyOptions } from "tunnel";
+import Url from "url";
 import uuid from "uuid/v1";
-import { AuthCredentials } from "../interfaces";
 
 class Client {
   private baseURL: string = process.env.KMDR_API_URL || "https://api.kmdr.sh/api/graphql";
@@ -11,30 +13,50 @@ class Client {
   private sessionId: string;
   private instance: AxiosInstance;
   private version: string;
+  private isHttps: boolean;
 
-  constructor(version: string, axiosInstance?: AxiosInstance, auth?: AuthCredentials) {
+  constructor(version: string, axiosInstance?: AxiosInstance) {
     this.sessionId = uuid();
     this.shell = process.env.SHELL || "";
     this.os = `${os.platform()} ${os.release()}`;
     this.term = `${process.env.TERM};${process.env.TERM_PROGRAM}`;
     this.version = version;
+    this.isHttps = this.baseURL.startsWith("https:");
 
-    this.instance =
-      axiosInstance ||
-      axios.create({
-        baseURL: this.baseURL,
-        headers: {
-          "Content-Type": "application/json",
-          "X-kmdr-client-os": this.os,
-          "X-kmdr-client-session-id": this.sessionId,
-          "X-kmdr-client-shell": this.shell,
-          "X-kmdr-client-term": this.term,
-          "X-kmdr-client-version": this.version,
-        },
-        responseType: "json",
-      });
+    const axiosConfig: AxiosRequestConfig = {
+      baseURL: this.baseURL,
+      headers: {
+        "Content-Type": "application/json",
+        "X-kmdr-client-os": this.os,
+        "X-kmdr-client-session-id": this.sessionId,
+        "X-kmdr-client-shell": this.shell,
+        "X-kmdr-client-term": this.term,
+        "X-kmdr-client-version": this.version,
+      },
+      responseType: "json",
+    };
+
+    const proxyEnv = this.isHttps ? "https_proxy" : "http_proxy";
+    let proxyUrl = process.env[proxyEnv] || process.env[proxyEnv.toUpperCase()];
+
+    if (this.isHttps && proxyUrl) {
+      proxyUrl = proxyUrl.startsWith("http") ? proxyUrl : "http://" + proxyUrl;
+      const parsedProxyUrl = Url.parse(proxyUrl);
+
+      const proxy: ProxyOptions = {
+        host: parsedProxyUrl.hostname || "",
+        port: parseInt(parsedProxyUrl.port || "", 10),
+        proxyAuth: parsedProxyUrl.auth,
+      };
+
+      let httpsAgent: Agent;
+      httpsAgent = Tunnel.httpsOverHttp({ proxy });
+      axiosConfig.httpsAgent = httpsAgent;
+      axiosConfig.proxy = false;
+    }
+
+    this.instance = axiosInstance || axios.create(axiosConfig);
   }
-
   protected doQuery(query: string, variables?: {}, config?: AxiosRequestConfig) {
     return this.post({ query, variables }, config);
   }
