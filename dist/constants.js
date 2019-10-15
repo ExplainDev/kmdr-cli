fbfff77f7df555693be8f5a8c2772e4b3a1cbe40
Eddie Ramirez <djedi.r@gmail.com>
2019-10-14 19:08:44 -0700

    Ignore dist/ directory

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// tslint:disable-next-line: no-var-requires
-const pkg = require("../package.json");
-exports.KMDR_CLI_VERSION = pkg.version;
-//# sourceMappingURL=constants.js.map
--- dist/explain/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const explain_1 = require("./explain");
-exports.Explain = explain_1.Explain;
-//# sourceMappingURL=index.js.map
